05674951bc01836b6189d9a6782d478284503eee
hriteek <hriteek.b@g-mana.com>
2025-07-28 15:50:46 +0545

    relace httpie

--- script/brew.sh
+++ script/brew.sh
@@ -1,2 +1,2 @@
 # install app with brew
-brew install bat commitizen entr eza fd fzf fnm gdu gh htop httpie jq neovim ripgrep tldr trash-cli unzip yq yazi zoxide
+brew install bat commitizen entr eza fd fzf fnm gdu gh htop xh jq neovim ripgrep tldr trash-cli unzip yq yazi zoxide
